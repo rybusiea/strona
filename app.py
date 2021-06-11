a3316918f2c405584dee30dcfe5729d30757eaef
rybusiea <agata.rybusiewicz@windowslive.com>
2021-06-11 16:40:20 +0200

    animacje przy scrollowaniu

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/projekty')
+def projekty():
+    return render_template('projekty.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
